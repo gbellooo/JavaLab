b62e536d9d5a60128b6185e11fd0e2a9fc513734
Gabriel Bello <125406298+gbellooo@users.noreply.github.com>
2025-02-19 00:16:40 +0000

    oi

---
+++ Lab2/src/ContaInfinita.java
@@ -0,0 +1,17 @@
+public class ContaInfinita extends Conta {
+    public ContaInfinita(int n, double s) {
+        super(n, s);
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor + 5;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        saldo -= valor;
+
+        return true;
+    }
+}
